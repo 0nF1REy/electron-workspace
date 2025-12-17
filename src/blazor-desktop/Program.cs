13406613d2b4c36d79e59f67459d0dc869a215f7
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-17 11:17:36 -0300

    main: feat (blazor-desktop) - Added Electron manifest and enabled Electron integration
    - Added an Electron manifest and updates the Blazor desktop project and launch configuration to support running the app under Electron.
    - Ensures the manifest is included in the build output and that the application initializes an Electron window when launched in an Electron environment.

--- src/blazor-desktop/Program.cs
+++ src/blazor-desktop/Program.cs
@@ -1,27 +1,37 @@
-using blazor_desktop.Components;
+using ElectronNET.API;
+using BlazorApp = blazor_desktop.Components.App;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Electron precisa saber que está rodando com ele
+builder.WebHost.UseElectron(args);
+
+// Adicionado os serviços ao contêiner.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurado o pipeline de requisições HTTP.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
+
+app.MapRazorComponents<BlazorApp>()
     .AddInteractiveServerRenderMode();
 
-app.Run();
+// Electron “entra em ação”
+if (HybridSupport.IsElectronActive)
+{
+    await Electron.WindowManager.CreateWindowAsync();
+}
+
+await app.RunAsync();
